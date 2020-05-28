8a59e678acc64191996f6d41dc5bf468de245119
Mikko Kautto <mikko.kautto@codento.com>
2020-05-28 13:25:48 +0300

    Add Highcharts and initial chart with static test data

---
+++ src/components/SearchPage.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import Boost from 'highcharts/modules/boost';
+
+import testdata from './pamb0.json';
+
+Boost(Highcharts);
+
+const foo = testdata.data.map(entry => entry["HYY_META.Pamb0"]);
+
+const options: Highcharts.Options = {
+  title: {
+      text: 'My chart'
+  },
+  plotOptions: {
+    series: {
+      boostThreshold: 1
+    }
+  },
+  series: [{
+      type: 'line',
+      data: foo
+  }]
+}
+
+const SearchPage: React.FC = (props: HighchartsReact.Props) => {
+  return (
+    <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        {...props}
+    />
+  )
+}
+
+export default SearchPage
