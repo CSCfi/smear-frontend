f6c637e05b384190e1908c6f418fb6fd6f26b944
Mikko Kautto <mikko.kautto@codento.com>
2020-06-03 15:02:34 +0300

    Initial version of data chart

--- src/components/search/TimeSeriesChart.tsx
+++ src/components/search/TimeSeriesChart.tsx
@@ -1,32 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import * as Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import Boost from 'highcharts/modules/boost'
-
-import testdata from './pamb0.json'
+import { timeSeriesSelector } from '../../store/timeseries'
+import { DataPoint } from '../../types'
 
 Boost(Highcharts)
 
-const foo = testdata.data.map((entry) => entry['HYY_META.Pamb0'])
-
-const options: Highcharts.Options = {
-  title: {
-    text: 'SMEAR data',
-  },
-  plotOptions: {
-    series: {
-      boostThreshold: 1,
-    },
-  },
-  series: [
-    {
-      type: 'line',
-      data: foo,
-    },
-  ],
-}
-
 const TimeSeriesChart: React.FC = (props: HighchartsReact.Props) => {
+  const timeSeries = useSelector(timeSeriesSelector)
+  const data = timeSeries.data.map((dataPoint: DataPoint) => dataPoint[timeSeries.columns[0]])
+  const options: Highcharts.Options = {
+    title: {
+      text: 'SMEAR data',
+    },
+    plotOptions: {
+      series: {
+        boostThreshold: 1,
+      },
+    },
+    series: [
+      {
+        type: 'line',
+        data,
+      },
+    ],
+  }
+
   return <HighchartsReact highcharts={Highcharts} options={options} {...props} />
 }
 
