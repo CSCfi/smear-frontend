e033fec94cf348c735c40a7f197e1fbde9c66b0a
Mikko Kautto <mikko.kautto@codento.com>
2020-06-10 15:20:25 +0300

    Read api url from .env

--- src/service/initialload/index.ts
+++ src/service/initialload/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { AppDispatch } from '../../store/index'
 import { setMenuItems } from '../../store/menuitems'
+import { API_URL, PATH_STRUCTURE } from '../../constants'
 
 export const fetchInitialData = () => {
   return async (dispatch: AppDispatch) => {
     return axios
-      .get('/structure')
+      .get(API_URL + PATH_STRUCTURE)
       .then((response) => {
         dispatch(setMenuItems(response.data))
       })
