57d547e67f6fade7ea0ab3bdb4f9c2c9eff091f9
Mikko Kautto <mikko.kautto@codento.com>
2020-06-10 15:35:03 +0300

    Use new format for chart data

--- src/components/search/TimeSeriesGrid.tsx
+++ src/components/search/TimeSeriesGrid.tsx
@@ -3,15 +3,13 @@ import { useSelector } from 'react-redux'
 import TimeSeriesChart from './TimeSeriesChart'
 import { Row, Col } from 'antd'
 import { timeSeriesSelector } from '../../store/timeseries'
-import { DataPoint } from '../../types'
 
 const TimeSeriesGrid: React.FC = () => {
   const timeSeries = useSelector(timeSeriesSelector)
-  const charts = timeSeries.columns.map((column) => {
-    const data = timeSeries.data.map((dataPoint: DataPoint) => dataPoint[column] as number)
+  const charts = Object.keys(timeSeries).map((variableName) => {
     return (
-      <Col key={column} span={8}>
-        <TimeSeriesChart name={column} data={data} />
+      <Col key={variableName} span={8}>
+        <TimeSeriesChart name={variableName} data={timeSeries[variableName]} />
       </Col>
     )
   })
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const API_URL = `${process.env.REACT_APP_API_URL}`
 export const PATH_STRUCTURE = '/structure'
-export const PATH_TIME_SERIES = '/search/timeseries'
+export const PATH_TIME_SERIES = '/search/timeseries/chart'
