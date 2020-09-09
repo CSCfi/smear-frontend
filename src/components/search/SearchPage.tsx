5b7d009d6e93eb2cbc658c8b4016000280d48445
Antero Vainio <antero.vainio@csc.fi>
2020-09-09 07:33:24 +0000

    Update SearchPage styling

--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout } from 'antd'
 import TreeMenu from './TreeMenu'
 import SearchControls from './SearchControls'
 import TimeSeriesGrid from './TimeSeriesGrid'
 
 const SearchPage: React.FC = () => {
+  useEffect(() => {
+    document.title = "AVAA - Search"
+  }, [])
+
   return (
     <Layout>
       <Layout>
-        <Layout.Header>SMEAR</Layout.Header>
+        <Layout.Header>
+          <img src="company_logo.png" alt="Smart" />
+        </Layout.Header>
       </Layout>
       <Layout>
         <Layout.Sider>
