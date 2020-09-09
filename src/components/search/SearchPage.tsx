686d151ce5a843c1da1b012acbf65adbc504fbcd
Antero Vainio <antero.vainio@csc.fi>
2020-09-09 08:29:20 +0000

    Add tooltips to SearchPage

--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react'
-import { Layout } from 'antd'
+import { Layout, Tooltip } from 'antd'
 import TreeMenu from './TreeMenu'
 import SearchControls from './SearchControls'
 import TimeSeriesGrid from './TimeSeriesGrid'
+
+const VARIABLES_TOOLTIP_TEXT = "You may choose stations and variables on the "
+  + "left. Clicking a triangle next to station name opens a list of variables "
+  + "divided into different categories. Some variables are measured with "
+  + "several instruments, (2) after name indicates secondary measurement. You "
+  + "may select multiple variables."
 
 const SearchPage: React.FC = () => {
   useEffect(() => {
@@ -18,7 +24,9 @@ const SearchPage: React.FC = () => {
       </Layout>
       <Layout>
         <Layout.Sider>
-          <span><b>Variables:</b></span>
+          <Tooltip placement="rightBottom" title={VARIABLES_TOOLTIP_TEXT}>
+            <span><b>Variables:</b></span>
+          </Tooltip>
           <TreeMenu />
         </Layout.Sider>
         <Layout.Content>
