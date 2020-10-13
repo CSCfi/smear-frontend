518b9ad9cb2100fc64822527a482dc36779cc392
Antero Vainio <antero.vainio@csc.fi>
2020-10-13 10:14:33 +0300

    Move search form functionality to DownloadSider component

--- src/components/download/DownloadSider.tsx
+++ src/components/download/DownloadSider.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Layout } from 'antd'
+import moment, { Moment } from 'moment'
 
 import {
   CategorySelect,
@@ -15,84 +16,95 @@ interface DownloadSiderProps {
   aggregations: any[],
   qualities: any[],
   stations: any[],
-  selectedStation: any,
-  onSelectStation(event: any): void,
-  selectedCategory: any,
-  onSelectCategory(event: any): void
-  selectedDateRange: any,
-  onSelectDateRange(event: any): void,
-  selectedQuality: any,
-  onSelectQuality: (value: any) => void,
-  selectedAggregation: any,
-  onSelectAggregation: (value: any) => void,
-  selectedAveraging: any,
-  onSelectAveraging: (value: any) => void,
-  selectedFilter: string,
-  onChangeFilter: (value: any) => void,
-  selectedFilterConditions: any[],
-  onChangeFilterConditions: (value: any) => void,
-  onUpdateClick: (value: any) => void
+  onUpdateClick: (
+    selectedStation: any,
+    selectedCategory: any,
+    selectedDateRange: any,
+    selectedQuality: any,
+    selectedAggregation: any,
+    selectedAveraging: any,
+    selectedFilter: any,
+    selectedFilterConditions: any
+  ) => void
 }
 
 const DownloadSider: React.FC<DownloadSiderProps> = ({
   aggregations,
   qualities,
   stations,
-  selectedStation,
-  onSelectStation,
-  selectedCategory,
-  onSelectCategory,
-  selectedDateRange,
-  onSelectDateRange,
-  selectedQuality,
-  onSelectQuality,
-  selectedAggregation,
-  onSelectAggregation,
-  selectedAveraging,
-  onSelectAveraging,
-  selectedFilter,
-  onChangeFilter,
-  selectedFilterConditions,
-  onChangeFilterConditions,
   onUpdateClick
 }) => {
+  const [selectedStation, setSelectedStation] = useState<any>()
+  const [selectedCategory, setSelectedCategory] = useState<any>()
+  const [selectedDateRange, setDateRange] = useState<Moment[]>([moment().subtract(1, "day"), moment()])
+  const [selectedQuality, setSelectedQuality] = useState<any>()
+  const [selectedAggregation, setSelectedAggregation] = useState<any>()
+  const [selectedAveraging, setSelectedAveraging] = useState<any>(30)
+  const [selectedFilter, setSelectedFilter] = useState<any>()
+  const [selectedFilterConditions, setSelectedFilterConditions] = useState<any[]>([])
+
+  const handleSelectStation = (event: any) => {
+    const station = event.target.value
+    setSelectedStation(station)
+    setSelectedCategory(station.children[0])
+  }
+  const handleSelectCategory = (value: any) =>
+    setSelectedCategory(selectedStation.children
+                        .find((category: any) => category.key === value))
+  const handleDateRangeChange = (value: any) => setDateRange(value as Moment[])
+  const handleQualityChange = (value: any) => setSelectedQuality(value)
+  const handleAveragingChange = (value: any) => setSelectedAveraging(value)
+  const handleAggregationChange = (value: any) => setSelectedAggregation(value)
+  const handleFilterChange = (event: any) => setSelectedFilter(event.target.value)
+  const handleFilterConditionChange = (value: any) => setSelectedFilterConditions(value)
+  const handleUpdateClick = (value: any) => onUpdateClick(
+    selectedStation,
+    selectedCategory,
+    selectedDateRange,
+    selectedQuality,
+    selectedAggregation,
+    selectedAveraging,
+    selectedFilter,
+    selectedFilterConditions,
+  )
+
   return (
     <Layout.Sider>
       <StationRadio
         stations={stations}
         selectedStation={selectedStation}
-        onSelectStation={onSelectStation}
+        onSelectStation={handleSelectStation}
       />
       <CategorySelect
         station={selectedStation}
         selectedCategory={selectedCategory}
-        onSelectCategory={onSelectCategory}
+        onSelectCategory={handleSelectCategory}
       />
       <DateRangePicker
         selectedDateRange={selectedDateRange}
-        onSelectDateRange={onSelectDateRange}
+        onSelectDateRange={handleDateRangeChange}
       />
       <QualitySelect
         qualities={qualities}
         selectedQuality={selectedQuality}
-        onSelectQuality={onSelectQuality}
+        onSelectQuality={handleQualityChange}
       />
       <AveragingInput
         selectedAveraging={selectedAveraging}
-        onSelectAveraging={onSelectAveraging}
+        onSelectAveraging={handleAveragingChange}
       />
       <AggregationSelect
         aggregations={aggregations}
         selectedAggregation={selectedAggregation}
-        onSelectAggregation={onSelectAggregation}
+        onSelectAggregation={handleAggregationChange}
       />
       <FilterInput
         selectedFilter={selectedFilter}
-        onChangeFilter={onChangeFilter}
+        onChangeFilter={handleFilterChange}
         selectedFilterConditions={selectedFilterConditions}
-        onChangeFilterConditions={onChangeFilterConditions}
+        onChangeFilterConditions={handleFilterConditionChange}
       />
-      <Button type="primary" onClick={onUpdateClick}>Update</Button>
+      <Button type="primary" onClick={handleUpdateClick}>Update</Button>
     </Layout.Sider>
   )
 }
