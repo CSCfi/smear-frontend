19cdcde7958b553a71d48bd6e92620bb6b51f9bf
Mattias Levlin <mattias.levlin@gmail.com>
2021-09-10 10:42:54 +0300

    Add "Contact" page and fix the INAR link

---
+++ src/components/contact/ContactPage.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { Layout } from 'antd'
+
+const ContactPage: React.FC = () => {
+  useEffect(() => {
+    document.title = "SmartSMEAR - Contact"
+  }, [])
+
+  return (
+    <Layout>
+      <Layout.Content>
+      <h1>Site support</h1>
+        <p>
+
+          For questions and issues regarding the functionality of the site, please contact <a href='mailto:servicedesk@csc.fi'>servicedesk@csc.fi</a>
+        </p>
+        <h1>Data support</h1>
+        <p>
+
+          For questions regarding the data, users are encouraged to contact the {' '}
+          <a href='https://wiki.helsinki.fi/display/SMEAR/Data+contact+persons'>
+            original data contributors
+          </a> or <a href='mailto:atm-data@helsinki.fi'>atm-data@helsinki.fi</a>
+          {' '} for questions or clarifications. More detailed information on the
+          measurements is provided at{' '}
+          <a href='https://wiki.helsinki.fi/display/SMEAR/The+SMEAR+Wikispace'>
+            SMEAR wikispace
+          </a>.
+        </p>
+      </Layout.Content>
+    </Layout>
+  )
+}
+
+export default ContactPage
