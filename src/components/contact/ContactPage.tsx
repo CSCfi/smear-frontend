28881765e6c7a19aa537368763133b3c14391794
Antero Vainio <antero.vainio@csc.fi>
2022-02-16 19:05:59 +0200

    Open external links to new tabs

--- src/components/contact/ContactPage.tsx
+++ src/components/contact/ContactPage.tsx
@@ -14,19 +14,22 @@ const ContactPage: React.FC = () => {
         <Layout.Content className="AppContainer">
         <h1>Site support</h1>
           <p>
-
-            For questions and issues regarding the functionality of the site, please contact <a href='mailto:servicedesk@csc.fi'>servicedesk@csc.fi</a>
+            For questions and issues regarding the functionality of the site, please contact{' '}
+            <a href='mailto:servicedesk@csc.fi'>servicedesk@csc.fi</a>
           </p>
           <h1>Data support</h1>
           <p>
-
-            For questions regarding the data, users are encouraged to contact the {' '}
-            <a href='https://wiki.helsinki.fi/display/SMEAR/Data+contact+persons'>
+            For questions regarding the data, users are encouraged to contact the{' '}
+            <a href='https://wiki.helsinki.fi/display/SMEAR/Data+contact+persons'
+               target='_blank'
+               rel='noopener noreferrer'>
               original data contributors
             </a> or <a href='mailto:atm-data@helsinki.fi'>atm-data@helsinki.fi</a>
             {' '} for questions or clarifications. More detailed information on the
             measurements is provided at{' '}
-            <a href='https://wiki.helsinki.fi/display/SMEAR/The+SMEAR+Wikispace'>
+            <a href='https://wiki.helsinki.fi/display/SMEAR/The+SMEAR+Wikispace'
+               target='_blank'
+               rel='noopener noreferrer'>
               SMEAR wikispace
             </a>.
           </p>
