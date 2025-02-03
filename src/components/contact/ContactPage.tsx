f3e96c402876dc47d9907f34b677f8034260d1f4
Roope Varttila <roope.varttila@csc.fi>
2025-02-03 09:12:29 +0000

    Update wiki links

--- src/components/contact/ContactPage.tsx
+++ src/components/contact/ContactPage.tsx
@@ -20,14 +20,14 @@ const ContactPage: React.FC = () => {
           <h1>Data support</h1>
           <p>
             For questions regarding the data, users are encouraged to contact the{' '}
-            <a href='https://wiki.helsinki.fi/display/SMEAR/Data+contact+persons'
+            <a href='https://wiki.helsinki.fi/xwiki/bin/view/SMEAR/The%20SMEAR%20Wikispace/SMEAR%20database/Data%20contact%20persons'
                target='_blank'
                rel='noopener noreferrer'>
               original data contributors
             </a> or <a href='mailto:atm-data@helsinki.fi'>atm-data@helsinki.fi</a>
             {' '} for questions or clarifications. More detailed information on the
             measurements is provided at{' '}
-            <a href='https://wiki.helsinki.fi/display/SMEAR/The+SMEAR+Wikispace'
+            <a href='https://wiki.helsinki.fi/xwiki/bin/view/SMEAR/The%20SMEAR%20Wikispace'
                target='_blank'
                rel='noopener noreferrer'>
               SMEAR wikispace
